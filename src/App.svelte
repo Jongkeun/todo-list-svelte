611a48bfe87ee7adaadb2e6e13c4f188e45383a5
Jongkeun <jongkeun.kr@gmail.com>
2020-01-23 17:57:25 +0900

    add initalize directory

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import H3 from "./components/H3";
+  import Article from "./components/Article";
+  import MainContainer from "./containers/MainContainer";
+  export let name = "like";
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<MainContainer>
+  <h1>Hello {name}!</h1>
+  <Article {name} />
+  <Article {name} />
+  <Article {name} />
+  <Article {name} />
+  <Article {name} />
+  <H3 text={name} />
+</MainContainer>
---
+++ src/containers/MainContainer.svelte
@@ -0,0 +1,11 @@
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
+
+<main>
+  <slot />
+</main>
