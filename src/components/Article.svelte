2e37aaf11adeb452f757c172dacdf49e221ebf22
Jongkeun <dldkfod@naver.com>
2020-01-24 23:06:22 +0900

    add css of article and homepage address

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -2,4 +2,17 @@
   export let name;
 </script>
 
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100;
+    max-width: 600px;
+    background-color: #fffcfc;
+    box-shadow: 0 10px 20px rgba(0 0 0 0.19) 0 6px 6px rgba(0 0 0 0.23);
+    margin: 10px 0;
+  }
+</style>
+
 <div>Article {name}</div>
